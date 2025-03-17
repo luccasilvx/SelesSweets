29e2bacd796f7155ed19aae61799dcc4b9fdad08
luccasilvx <lucafreitasilva@gmail.com>
2025-03-17 15:16:30 -0300

    subindo projeto

---
+++ SelesSweets/src/main/java/com/leads/SelesSweets/SelesSweetsApplication.java
@@ -0,0 +1,13 @@
+package com.leads.SelesSweets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelesSweetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelesSweetsApplication.class, args);
+	}
+
+}
